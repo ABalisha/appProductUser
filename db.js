7662c8699fcab6089126d1a55633a8b6d3ac2a66
ABalisha <arditbalisha01@gmail.com>
2021-10-11 21:37:59 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
 mongoose
-.connect("mongodb+srv://AB:Rj2tYHOnCRyKAsKD@cluster0.3l0cm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+.connect(process.env.db)
 .then(res=> console.log("connected succ"))
 .catch(err=> console.log(err))

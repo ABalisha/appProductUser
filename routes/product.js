f440a80cd1cbffafe8f9f233bd5c1db8db2e91ef
ABalisha <arditbalisha01@gmail.com>
2021-10-14 13:35:09 +0200

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,21 +1,40 @@
-const express = require('express')
+const express = require("express");
 const router = express();
-const path = require('path')
-const {allproducts1} = require('../controller/helpers/index')
-const {mainroute,allproducts,addProduct,removeProduct,delet} = require('../controller/product')
-const product = require('../models/product')
-const {allproducts123,allproductsdelete,allproductspost, allproductsupdate} = require('../controller/allproductsjson')
-require('dotenv/config')
+const path = require("path");
+const { allproducts1 } = require("../controller/helpers/index");
+const {
+  mainroute,
+  allproducts,
+  addProduct,
+  removeProduct,
+  delet,
+  
+} = require("../controller/product");
+const product = require("../models/product");
+const {
+  allproducts123,
+  allproductsdelete,
+  allproductspost,
+  allproductsupdate,
+  getproductbyid
+} = require("../controller/allproductsjson");
+require("dotenv/config");
 //ROUTES add product get
-router.get('/', mainroute);
-router.get('/all-products', allproducts)
-router.get('/all-products/json', allproducts123)
-router.delete('/all-products/json', allproductsdelete)
-router.post('/all-products/json',allproductspost)
-router.put('/all-products/json',allproductsupdate)
+router.get("/", mainroute);
+router.get("/all-products", allproducts);
+
+// API PRODUCTS -------------
+router
+  .route("/all-products/json")
+  .get(allproducts123)
+  .post(allproductspost)
+  .delete(allproductsdelete)
+  .put(allproductsupdate);
 // Post route to add product
-router.post('/add-product',addProduct)
+
+router.get("/all-products/json/:id", getproductbyid);
+router.post("/add-product", addProduct);
 // Remove a product based on ID
-router.post('/remove', removeProduct)
-router.get('/delet/:ProductName', delet)
+router.post("/remove", removeProduct);
+router.get("/delet/:ProductName", delet);
 module.exports = router;

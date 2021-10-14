5c3f51a0bdefe2876b6fc078a5da42ac814392ca
ABalisha <arditbalisha01@gmail.com>
2021-10-14 13:34:32 +0200

    Update allproductsjson.js

--- controller/allproductsjson.js
+++ controller/allproductsjson.js
@@ -3,9 +3,7 @@
     const product = require('../models/product')
     // allproducts 
     exports.allproducts123 = async (req,res)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+        if(!req.query){
     await product.find()
     .then(data=>{
         res.status(200).send(data)
@@ -16,12 +14,22 @@
         console.log(err)
         })
     }
+    else
+    {
+        await product.find(req.query)
+        .then(data=>{
+            res.status(200).send(data)
+        })
+        .catch(err=>
+            {
+                res.status(400).send(err)
+            console.log(err)
+            })
+    }
+    }
 // delete 
 
 exports.allproductsdelete = async (req,res)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     let todel = req.body
     console.log(todel)
     await product.deleteOne(todel)
@@ -38,9 +46,6 @@ exports.allproductsdelete = async (req,res)=>{
     // post 
 
     exports.allproductspost = async (req,res)=>{
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
         let todel = req.body
         console.log(todel)
         const add = new product(todel)
@@ -55,9 +60,6 @@ exports.allproductsdelete = async (req,res)=>{
             })
         }
         exports.allproductsupdate = async (req,res)=>{
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-            res.header('Access-Control-Allow-Headers', 'Content-Type');
             const id = req.query.id
             const productTitle = req.body.productTitle;
             const productPrize = req.body.productPrize;
@@ -65,4 +67,16 @@ exports.allproductsdelete = async (req,res)=>{
                 if(err) {res.status(400).send(err)}
                 res.status(200).send("updated user:" + data)
             })
+            }
+
+            exports.getproductbyid = async (req,res,next)=>{
+                await product.find({_id:req.params.id},(err,data)=>{
+                    if(err){
+                        console.log(err)
+                    }
+                    else{
+                        res.status(200).send(data)
+                        next()
+                    }
+                })
             }

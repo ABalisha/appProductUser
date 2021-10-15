1053579920df4be165b755bf7fce00c5a5a802a8
ABalisha <arditbalisha01@gmail.com>
2021-10-15 21:43:49 +0200

    Update deviceLogging.js

--- controller/helpers/deviceLogging.js
+++ controller/helpers/deviceLogging.js
@@ -1,54 +1,32 @@
-const {ipLog} = require('../../models/iplogging')
-const os = require('os');
-const ip = require('../../node_modules/ipaddressapi')
-exports.deviceLogging = async (req,res,next)=>
-{
-    const urlR = req.baseUrl + req.path
-    const newVisit = new ipLog({
-        Ip:await ip.address(),
-        Enviroment:os.platform(),
-        Hostname:os.hostname(),
-        Namespace:urlR,
+const { ipLog } = require("../../models/iplogging");
+const os = require("os");
+const ip = require("../../node_modules/ipaddressapi");
+const namespace = "deviceLogging";
+const { info, warn, error } = require("../../logger");
+const test = async ()=>{
+  await ipLog.find()
+  .then((result)=>{
+    if(result.length > 25){
+    ipLog.findByIdAndDelete(result[0]._id)
+    .then((data)=>{
+        info(namespace,"Data That was deleted", data)
     })
-    newVisit.save()
-    next()
+    .catch((err)=>{
+error(namespace,"Error",err)
+    })
+    }
+  })
 }
-
-
-// const { ipLog } = require("../../models/iplogging");
-// const os = require("os");
-// const ip = require("../../node_modules/ipaddressapi");
-// const namespace = "deviceLogging";
-// const { info, warn, error } = require("../../logger");
-// const test = async ()=>{
-//   await ipLog.find((err, result) => {
-//     if (result.length > 3) {
-//       try {
-//         ipLog.findByIdAndDelete(result[result.length - 1]._id, (err, doc) => {
-//           if (err) {
-//             error(namespace, "ERROR", err);
-//           } else {
-//             info(namespace, "Deleted The Following", doc);
-//           }
-//         });
-//       } catch (e) {
-//         error(namespace, "Error", e);
-//       }
-//     }
-//     if (err) {
-//       error(namespace, "Data Length", error);
-//     }
-//   });
-// }
-// exports.deviceLogging = async (req, res, next) => {
-// await test()
-//   const urlR = req.baseUrl + req.path;
-//   const newVisit = new ipLog({
-//     Ip: await ip.address(),
-//     Enviroment: os.platform(),
-//     Hostname: os.hostname(),
-//     Namespace: urlR,
-//   });
-//   newVisit.save();
-//   next();
-// };
+exports.deviceLogging = async (req, res, next) => {
+await test()
+  const urlR = req.baseUrl + req.path;
+  const newVisit = new ipLog({
+    Ip: await ip.address(),
+    Enviroment: os.platform(),
+    Hostname: os.hostname(),
+    Namespace: urlR,
+  });
+  newVisit.save();
+  info(namespace,"New Device Added",newVisit)
+  next();
+};

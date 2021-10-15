7565359f24ab6cfd979466173b69c2cabe55942d
ABalisha <arditbalisha01@gmail.com>
2021-10-15 17:50:48 +0200

    Update deviceLogging.js

--- controller/helpers/deviceLogging.js
+++ controller/helpers/deviceLogging.js
@@ -13,3 +13,42 @@ exports.deviceLogging = async (req,res,next)=>
     newVisit.save()
     next()
 }
+
+
+// const { ipLog } = require("../../models/iplogging");
+// const os = require("os");
+// const ip = require("../../node_modules/ipaddressapi");
+// const namespace = "deviceLogging";
+// const { info, warn, error } = require("../../logger");
+// const test = async ()=>{
+//   await ipLog.find((err, result) => {
+//     if (result.length > 3) {
+//       try {
+//         ipLog.findByIdAndDelete(result[result.length - 1]._id, (err, doc) => {
+//           if (err) {
+//             error(namespace, "ERROR", err);
+//           } else {
+//             info(namespace, "Deleted The Following", doc);
+//           }
+//         });
+//       } catch (e) {
+//         error(namespace, "Error", e);
+//       }
+//     }
+//     if (err) {
+//       error(namespace, "Data Length", error);
+//     }
+//   });
+// }
+// exports.deviceLogging = async (req, res, next) => {
+// await test()
+//   const urlR = req.baseUrl + req.path;
+//   const newVisit = new ipLog({
+//     Ip: await ip.address(),
+//     Enviroment: os.platform(),
+//     Hostname: os.hostname(),
+//     Namespace: urlR,
+//   });
+//   newVisit.save();
+//   next();
+// };

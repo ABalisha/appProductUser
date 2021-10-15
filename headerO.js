f10d1cdf8136dfe50c2f664d5a747667cf23c6ff
ABalisha <arditbalisha01@gmail.com>
2021-10-15 11:33:29 +0200

    Update headerO.js

--- headerO.js
+++ headerO.js
@@ -1,5 +1,5 @@
 exports.headeroptions = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "www.google.com");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();

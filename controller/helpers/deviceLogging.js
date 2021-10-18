cd5ec945e45a75c6b455a75a58e2e3e142979892
ABalisha <arditbalisha01@gmail.com>
2021-10-18 10:26:54 +0200

    Update deviceLogging.js

--- controller/helpers/deviceLogging.js
+++ controller/helpers/deviceLogging.js
@@ -1,6 +1,7 @@
 const { ipLog } = require("../../models/iplogging");
 const os = require("os");
 const ip = require("../../node_modules/ipaddressapi");
+const cookieParser = require("cookie-parser");
 const namespace = "deviceLogging";
 const { info, warn, error } = require("../../logger");
 const test = async ()=>{
@@ -18,15 +19,34 @@ error(namespace,"Error",err)
   })
 }
 exports.deviceLogging = async (req, res, next) => {
-await test()
+  var cookie = req.cookies.DeviceLoggingCookie
   const urlR = req.baseUrl + req.path;
+  info(namespace,"Cookie",typeof cookie)
+  if(typeof cookie === 'undefined' && !cookie){
+await test()
+  
   const newVisit = new ipLog({
     Ip: await ip.address(),
-    Enviroment: os.platform(),
+    Enviroment: os.type(),
     Hostname: os.hostname(),
     Namespace: urlR,
+    UserAgent: JSON.stringify(req.headers)
   });
+  var cookieNumber=Math.random().toString();
+  res.cookie('DeviceLoggingCookie',cookieNumber, { maxAge: 90000, httpOnly: true });
   newVisit.save();
   info(namespace,"New Device Added",newVisit)
   next();
+}
+else{
+
+  const currentVisit = new ipLog({
+    Ip: await ip.address(),
+    Namespace: urlR,
+  UserAgent: JSON.stringify(req.headers)
+  })
+  currentVisit.save();
+  info(namespace,"Device that was already logged",currentVisit)
+  next()
+}
 };
